cdf61b6f342ab064a1d54d25456dd40d1236aba8
alouzizi <alouzizi@student.1337.ma>
2023-10-05 08:28:07 +0100

    Add City and Weather API call

---
+++ scripts/foreast.js
@@ -0,0 +1,26 @@
+const key = 'j0nL0tUnk0zUk9j2jtKBYpurVPk1UAu6'
+
+//get weather information
+const getWeather = async (id) => {
+	const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+	const query = `${id}?apikey=${key}`;
+	const response = await fetch(base + query);
+	const data = await response.json();
+	return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+
+	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	const query = `?apikey=${key}&q=${city}`;
+
+	const response = await fetch(base + query);
+	const data = await response.json();
+	return data[0];
+};
+
+getCity('london').then(data => {
+	return getWeather(data.Key);
+}).then(data => console.log(data))
+.catch(err => console.log(err));
